9e58d3f73dd9297548ffe68044387c5ad336f659
lenomatos <leno_matos@hotmail.com>
2022-07-09 00:59:44 -0300

    First realupdate

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Amora!
+        </p>
+        <p>
+          Carai!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
